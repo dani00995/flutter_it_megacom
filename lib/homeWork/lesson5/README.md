9a2245278bc64559c6bfc3d0c2558335a2b6288b
bigli <daniel.kg9526@gmail.com>
2023-01-16 16:46:34 +0600

    lesson 5. Date 16.01.2023

--- lib/homeWork/lesson5/README.md
+++ lib/homeWork/lesson5/README.md
@@ -2,32 +2,73 @@
 <img src="https://github.com/dani00995/flutter_it_megacom/blob/master/result/gif/animateButton.gif?raw=true" width="260" height="480">
 
 ```Dart
-@override
+class AppAnimatedButton extends StatelessWidget {
+  AppAnimatedButton({
+    Key? key,
+    required this.onPressed,
+    required this.experienceText,
+    required this.yearText,
+  }) : super(key: key);
+  final Function() onPressed;
+  final String experienceText;
+  final String yearText;
+  bool isClicked = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-    onTapDown: (_){
-      isClicked = !isClicked;
-      (context as Element).markNeedsBuild();
-    },
-    onTapUp: (_){
-      isClicked = !isClicked;
-      (context as Element).markNeedsBuild();
-      onPressed();
-
-    },
-    child: AnimatedContainer(
-      width: 98,
-      height: 108,
-      decoration: BoxDecoration(color: Colors.white, boxShadow: const [BoxShadow(color: Color.fromRGBO(0, 0, 0,0.06), offset: Offset(0,4),blurRadius: 15)],borderRadius: BorderRadius.circular(10),border: Border.all(color: isClicked? Colors.green: Colors.white, width: 3)),
-      duration: const Duration(milliseconds: 300),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center, children:  [
-        Text(experienceText, style: const TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.w600),textAlign: TextAlign.center,),
-        const SizedBox(height: 12,),
-        Text(yearText, style: const TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w600),textAlign: TextAlign.center,),
-        const Text('months', style: TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.w400),textAlign: TextAlign.center,),
-       
-      ]),
-    ),
-  );
+      onTapDown: (_) {
+        isClicked = !isClicked;
+        (context as Element).markNeedsBuild();
+      },
+      onTapUp: (_) {
+        isClicked = !isClicked;
+        (context as Element).markNeedsBuild();
+        onPressed();
+      },
+      child: AnimatedContainer(
+        width: 98,
+        height: 108,
+        decoration: decoration(),
+        duration: const Duration(milliseconds: 300),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              textWidget(experienceText),
+              const SizedBox(
+                height: 12,
+              ),
+              textWidget(yearText, size: 24),
+              textWidget('months', weight: FontWeight.w400)
+            ]),
+      ),
+    );
   }
+
+  //Text Widget method
+  Widget textWidget(String text,
+      {double size = 12, FontWeight weight = FontWeight.w600}) {
+    return Text(
+      text,
+      style: TextStyle(color: Colors.black, fontSize: size, fontWeight: weight),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  //Box decorotion method
+  BoxDecoration decoration() {
+    return BoxDecoration(
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.06),
+              offset: Offset(0, 4),
+              blurRadius: 15)
+        ],
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+            color: isClicked ? Colors.green : Colors.white, width: 3));
+  }
+}
   ```
--- lib/homeWork/lesson5/lesson_5.dart
+++ lib/homeWork/lesson5/lesson_5.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_it_megacom/const/AppColors.dart';
 
 class LessonFive extends StatelessWidget {
   const LessonFive({super.key});
@@ -11,11 +8,24 @@ class LessonFive extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffF9F9F9),
       body: Center(
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            AppAnimatedButton(onPressed: (){}, experienceText: 'Beginner', yearText: '0-6',),
-            AppAnimatedButton(onPressed: (){}, experienceText: 'Intermediate', yearText: '6-18',),
-            AppAnimatedButton(onPressed: (){}, experienceText: 'Advanced', yearText: '18+',),
+            AppAnimatedButton(
+              onPressed: () {},
+              experienceText: 'Beginner',
+              yearText: '0-6',
+            ),
+            AppAnimatedButton(
+              onPressed: () {},
+              experienceText: 'Intermediate',
+              yearText: '6-18',
+            ),
+            AppAnimatedButton(
+              onPressed: () {},
+              experienceText: 'Advanced',
+              yearText: '18+',
+            ),
           ],
         ),
       ),
@@ -25,44 +35,70 @@ class LessonFive extends StatelessWidget {
 
 class AppAnimatedButton extends StatelessWidget {
   AppAnimatedButton({
-    Key? key,required this.onPressed, required this.experienceText, required this.yearText ,
+    Key? key,
+    required this.onPressed,
+    required this.experienceText,
+    required this.yearText,
   }) : super(key: key);
   final Function() onPressed;
   final String experienceText;
   final String yearText;
   bool isClicked = false;
 
-  
-  
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-    onTapDown: (_){
-      isClicked = !isClicked;
-      (context as Element).markNeedsBuild();
-    },
-    onTapUp: (_){
-      isClicked = !isClicked;
-      (context as Element).markNeedsBuild();
-      onPressed();
-
-    },
-    child: AnimatedContainer(
-      width: 98,
-      height: 108,
-      decoration: BoxDecoration(color: Colors.white, boxShadow: const [BoxShadow(color: Color.fromRGBO(0, 0, 0,0.06), offset: Offset(0,4),blurRadius: 15)],borderRadius: BorderRadius.circular(10),border: Border.all(color: isClicked? Colors.green: Colors.white, width: 3)),
-      duration: const Duration(milliseconds: 300),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.center, children:  [
-        Text(experienceText, style: const TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.w600),textAlign: TextAlign.center,),
-        const SizedBox(height: 12,),
-        Text(yearText, style: const TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w600),textAlign: TextAlign.center,),
-        const Text('months', style: TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.w400),textAlign: TextAlign.center,),
-       
-      ]),
-    ),
-  );
+      onTapDown: (_) {
+        isClicked = !isClicked;
+        (context as Element).markNeedsBuild();
+      },
+      onTapUp: (_) {
+        isClicked = !isClicked;
+        (context as Element).markNeedsBuild();
+        onPressed();
+      },
+      child: AnimatedContainer(
+        width: 98,
+        height: 108,
+        decoration: decoration(),
+        duration: const Duration(milliseconds: 300),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              textWidget(experienceText),
+              const SizedBox(
+                height: 12,
+              ),
+              textWidget(yearText, size: 24),
+              textWidget('months', weight: FontWeight.w400)
+            ]),
+      ),
+    );
   }
 
-  
+  //Text Widget method
+  Widget textWidget(String text,
+      {double size = 12, FontWeight weight = FontWeight.w600}) {
+    return Text(
+      text,
+      style: TextStyle(color: Colors.black, fontSize: size, fontWeight: weight),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  //Box decorotion method
+  BoxDecoration decoration() {
+    return BoxDecoration(
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.06),
+              offset: Offset(0, 4),
+              blurRadius: 15)
+        ],
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+            color: isClicked ? Colors.green : Colors.white, width: 3));
+  }
 }
